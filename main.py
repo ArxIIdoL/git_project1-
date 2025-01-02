794563d0754f05949b85cfa410a7d0c7a8ecad3c
ariidol <aleksandrsergvol@gmail.com>
2025-01-02 17:21:07 +0300

    Жёлтые окружности

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('А ТЕПЕРЬ ПО РУССКИИ')
+import sys
+from random import randint
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for _ in range(randint(2, 6)):
+            radius, center_x, center_y = randint(5, 30), randint(60, 280), randint(60, 210)
+            qp.drawEllipse(center_x - radius, center_y - radius, 2 * radius, 2 * radius)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    form = MyWidget()
+    form.show()
+    sys.exit(app.exec())
